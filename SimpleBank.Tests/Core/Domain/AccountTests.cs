1f45c453ab30f380a4a124c627373b5b4cd8dc11
Deric <dericprado@gmail.com>
2024-04-27 15:22:03 -0300

    Teste de dominio

---
+++ SimpleBank.Tests/Core/Domain/AccountTests.cs
@@ -0,0 +1,28 @@
+﻿using AutoBogus;
+using FluentAssertions;
+using SimpleBank.Core.Domains.Entities;
+using SimpleBank.Core.Domains.Enums;
+using SimpleBank.Core.Domains.ValueObjects;
+
+namespace SimpleBank.Tests.Core.Domain;
+public class AccountTests
+{
+    public AccountTests() 
+    { 
+    }
+
+    [Fact]
+    public void FromCreateAccount_Should_SetPropieties()
+    {
+        //Arrange
+        var createAccount = AutoFaker.Generate<CreateAccount>();
+
+        //Act
+        var result = new Account().FromCreateAccount(createAccount);
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Status.Should().Be(Status.Active);
+        result.Balance.Should().Be(0.00M);
+    }
+}
