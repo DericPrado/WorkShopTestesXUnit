05c062a0448f76f46f5392e0705c65c77ee8619f
Deric <dericprado@gmail.com>
2024-04-28 20:36:19 -0300

    Criando testes para Transaction

---
+++ SimpleBank.Tests/Core/Domain/TransactionTests.cs
@@ -0,0 +1,31 @@
+﻿using AutoBogus;
+using FluentAssertions;
+using SimpleBank.Core.Domains.Entities;
+using SimpleBank.Core.Domains.ValueObjects;
+
+namespace SimpleBank.Tests.Core.Domain;
+
+public class TransactionTests
+{
+    public TransactionTests() 
+    {
+    }
+
+    [Fact]
+    [Trait("TransactionTests", "UnitTest")]
+    public async void FromCreateTransaction_Should_ReturnTransaction()
+    {
+        //Arrange
+        var createTransaction = AutoFaker.Generate<CreateTransaction>();
+        int cardId = 5;
+
+        //Act
+        var result = new Transaction().FromCreateTransaction(cardId, createTransaction);
+
+        //Assert
+        result.Should().NotBeNull();
+        result.TransactionType.Should().Be(createTransaction.TransactionType);
+        result.Amount.Should().Be(createTransaction.Amount);
+        result.CardId.Should().Be(cardId);
+    }
+}
